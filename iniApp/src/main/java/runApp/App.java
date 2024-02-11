79cbf9b724789e71b56aab4826c17ec0dcd5fb1a
unknown <fajardillah25@gmail.com>
2024-02-11 17:21:55 +0700

    Upload

---
+++ iniApp/src/main/java/runApp/App.java
@@ -0,0 +1,8 @@
+package runApp;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello saya fajar");
+    }
+}
---
+++ iniApp/src/test/java/runApp/appTest.java
@@ -0,0 +1,15 @@
+package runApp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class appTest {
+
+    @Test
+    public void ShouldAnswerWithTrue(){
+        Assert.assertTrue(true);
+    }
+
+
+
+}
